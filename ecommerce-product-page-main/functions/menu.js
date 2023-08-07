74475afa665b9b4d8e4e6e830b060fca55edf180
Elvis Josue <elvisjosue98@hotmail.com>
2023-08-06 20:48:16 -0600

    Ecommerce FIXED

--- ecommerce-product-page-main/functions/menu.js
+++ ecommerce-product-page-main/functions/menu.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** 
     CONSTANTES 
     PARA EL CONTENIDO 
@@ -19,26 +18,9 @@ const showMenu = () => {
 // CON ESTA FUNCION OCULTAREMOS EL MENU
 const closeMenu = () => {
     containerMenu.classList.remove('Show')
-=======
+}
 /** 
     CONSTANTES 
     PARA EL CONTENIDO 
     DEL MENU
 **/
-const containerMenu = document.querySelector('.Container__Menu')
-
-/** 
-    FUNCIONES PARA
-    LOS BOTONES RELACIONADOS
-    CON EL MENU
-**/
-
-// CON ESTA FUNCION MOSTRAREMOS EL MENU
-const showMenu = () => {
-    containerMenu.classList.add('Show')
-}
-// CON ESTA FUNCION OCULTAREMOS EL MENU
-const closeMenu = () => {
-    containerMenu.classList.remove('Show')
->>>>>>> 44ebaf7 (Chalenge completed!)
-}
