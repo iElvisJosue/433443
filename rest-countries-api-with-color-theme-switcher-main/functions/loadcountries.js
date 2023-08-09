a5c3b82720a7b8c768790f0b16d9ce977ad8d54d
Elvis Josue <elvisjosue98@hotmail.com>
2023-08-08 19:43:05 -0600

    API updated!

--- rest-countries-api-with-color-theme-switcher-main/functions/loadcountries.js
+++ rest-countries-api-with-color-theme-switcher-main/functions/loadcountries.js
@@ -58,7 +58,7 @@ let countries
 
 // CON ESTA FUNCION OBTENEMOS TODOS LOS PAISES DE LA API LOCAL
 async function loadCountries() {
-    const response = await fetch('./data.json')
+    const response = await fetch('https://raw.githubusercontent.com/iElvisJosue/Frontend_Mentor_Challenges/main/rest-countries-api-with-color-theme-switcher-main/api/data.json')
     countries = await response.json()
 
     showAllCountries()
